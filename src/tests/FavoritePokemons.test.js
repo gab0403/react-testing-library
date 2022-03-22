1d894a8441ef6cb2fd1b852b2db4edcc6a17ea98
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-03-22 17:57:46 -0300

    Requisito 3 - realizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  it('Teste se não tiver pokémon favorito é exibido No favorite pokemon found.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const favoritePokemonsTitle = screen.getByText('No favorite pokemon found');
+    expect(favoritePokemonsTitle).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', async () => {
+    renderWithRouter(<App />);
+
+    const details = await screen.findByRole('link', { name: /more details/i });
+    userEvent.click(details);
+
+    const checkFav = await screen.findByRole('checkbox', {
+      name: /Pokémon favoritado?/i,
+    });
+    userEvent.click(checkFav);
+
+    const favorite = await screen.findByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favorite);
+    const fav = screen.getByText('Pikachu');
+    expect(fav).toBeInTheDocument();
+  });
+});
