d31f25ad8a7ba97442f387f2cc8867e84d7972f5
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-03-22 15:44:09 -0300

    Requisito 1 - realizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa se o topo da aplic tem um conjunto fixo de links de navegação.', () => {
+  it('Verifica se o primeiro link possui o texto Home.', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+
+    const homeTitle = screen.getByRole('heading',
+      { name: 'Encountered pokémons' });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  it('Verifica se o primeiro link possui o texto About.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    const aboutTitle = screen.getByRole('heading',
+      { name: 'About Pokédex' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('Verifica se o primeiro link possui o texto Favorite Pokémons.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoritePokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoritePokemonsLink).toBeInTheDocument();
+    userEvent.click(favoritePokemonsLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+
+    const favoritePokemonsTitle = screen.getByRole('heading',
+      { name: 'Favorite pokémons' });
+    expect(favoritePokemonsTitle).toBeInTheDocument();
+  });
+});
