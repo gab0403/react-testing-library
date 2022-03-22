12442a7125a323d5d339f70f65ce66e2ac56439d
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-03-22 19:05:05 -0300

    Requisito 4 - realizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se contém um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji' });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex:', async () => {
+    renderWithRouter(<NotFound />);
+    const img = await
+    screen.findByAltText('Pikachu crying because the page requested was not found');
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(img.src).toMatch(url);
+  });
+});
